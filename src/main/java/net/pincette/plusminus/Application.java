5285883c5a5c293456558cbf10ea1a4655eff016
Werner Donné <werner.donne@pincette.biz>
2020-01-09 14:28:06 +0100

    Add Reactor example

--- src/main/java/net/pincette/plusminus/Application.java
+++ src/main/java/net/pincette/plusminus/Application.java
@@ -1,29 +1,43 @@
 package net.pincette.plusminus;
 
+import static com.mongodb.client.model.Aggregates.match;
+import static com.mongodb.client.model.Aggregates.project;
+import static com.mongodb.client.model.Filters.ne;
+import static com.mongodb.client.model.Projections.include;
 import static com.mongodb.reactivestreams.client.MongoClients.create;
 import static java.lang.System.exit;
 import static java.util.concurrent.CompletableFuture.completedFuture;
 import static java.util.logging.Level.parse;
 import static java.util.logging.Logger.getLogger;
 import static javax.json.Json.createObjectBuilder;
+import static javax.json.Json.createValue;
 import static net.pincette.jes.elastic.APM.monitor;
 import static net.pincette.jes.elastic.Logging.log;
 import static net.pincette.jes.util.Configuration.loadDefault;
+import static net.pincette.jes.util.Event.changed;
+import static net.pincette.jes.util.JsonFields.COMMAND;
+import static net.pincette.jes.util.JsonFields.ID;
+import static net.pincette.jes.util.Mongo.addNotDeleted;
+import static net.pincette.jes.util.Mongo.aggregationPublisher;
 import static net.pincette.jes.util.Streams.start;
+import static net.pincette.util.Collections.list;
 import static net.pincette.util.Util.tryToDoWithRethrow;
 import static net.pincette.util.Util.tryToGetSilent;
 
 import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoDatabase;
 import com.typesafe.config.Config;
 import java.util.concurrent.CompletionStage;
 import java.util.function.IntUnaryOperator;
 import java.util.logging.Level;
 import javax.json.JsonObject;
 import net.pincette.jes.Aggregate;
+import net.pincette.jes.Reactor;
 import net.pincette.jes.util.Fanout;
 import net.pincette.jes.util.Streams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
+import org.reactivestreams.Publisher;
 
 /**
  * A demo application for pincette-jes.
@@ -96,6 +110,31 @@ public class Application {
     return aggregate.build();
   }
 
+  private static CompletionStage<JsonObject> createCommand(final JsonObject event) {
+    return completedFuture(createObjectBuilder().add(COMMAND, "plus").build());
+  }
+
+  @SuppressWarnings("squid:UnusedPrivateMethod") // Reactor example.
+  private static StreamsBuilder createReactor(
+      final StreamsBuilder builder, final Config config, final MongoClient mongoClient) {
+    final String environment = getEnvironment(config);
+
+    return new Reactor()
+        .withBuilder(builder)
+        .withEnvironment(environment)
+        .withSourceType("plusminus-counter")
+        .withDestinationType("plusminus-counter")
+        .withDestinations(
+            event ->
+                getOthers(
+                    event.getString(ID),
+                    mongoClient.getDatabase(config.getString(MONGODB_DATABASE)),
+                    environment))
+        .withEventToCommand(Application::createCommand)
+        .withFilter(event -> changed(event, "/value", createValue(9), createValue(10)))
+        .build();
+  }
+
   static String getEnvironment(final Config config) {
     return tryToGetSilent(() -> config.getString(ENVIRONMENT)).orElse(DEV);
   }
@@ -106,6 +145,13 @@ public class Application {
 
   static MongoClient getMongoClient(final Config config) {
     return create(config.getString(MONGODB_URI));
+  }
+
+  private static Publisher<JsonObject> getOthers(
+      final String id, final MongoDatabase database, final String environment) {
+    return aggregationPublisher(
+        database.getCollection("plusminus-counter-" + environment),
+        list(match(addNotDeleted(ne(ID, id))), project(include(ID))));
   }
 
   private static CompletionStage<JsonObject> reduce(
